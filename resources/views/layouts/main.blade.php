7d7608d1a952e105384463d5a38519c04bacf043
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-06-14 11:17:38 +0700

    add //maxcdn.bootstrapcdn.com/ to use bootstrap in heroku

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Laravel 5.5 : @yield('page_title')</title>
-	{{ Html::style(('bootstrap3\css\bootstrap.min.css')) }}
+	{{ Html::style(('//maxcdn.bootstrapcdn.com/bootstrap3\css\bootstrap.min.css')) }}
 	{{--
 	@if(isset($style))
 		@foreach ($style as $css)
@@ -17,9 +17,9 @@
 		@yield('content')
 
 	</div>
-	{{ Html::script(('js\jquery.min.js')) }}
-	{{ Html::script(('js\blog\app.js')) }}
-	{{ Html::script(('bootstrap3\js\bootstrap.min.js')) }}
+	{{ Html::script(('//maxcdn.bootstrapcdn.com/js\jquery.min.js')) }}
+	{{ Html::script(('//maxcdn.bootstrapcdn.com/js\blog\app.js')) }}
+	{{ Html::script(('//maxcdn.bootstrapcdn.com/bootstrap3\js\bootstrap.min.js')) }}
 
 </body>
 </html>
