199a17c3b8c52beb9bce774e559d40cba2344ef5
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-07-11 18:57:51 +0700

    header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,13 +7,15 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-default">
+<nav class="navbar navbar-default">
   <div class="container">
 		{{-- {{ Html::link('songkorea/','', style="max-width: 150px;", Html::image(asset('images\YK48logo.png'))) }} --}}
     <img src="images\YK48logo.png" class="img-responsive" style="max-width: 150px;">
   </div>
 </nav>
-	<div class="main-container">
+
+
+<div class="main-container">
 		<div class="container">
 		@yield('content')
 		</div>
@@ -23,15 +25,16 @@
 	{{ Html::script(('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js')) }}
 
 </body>
+
 <footer>
 	<div class="footer">
 		<nav class="navbar navbar-default" style="margin: 0;">
 	    <div class="container">
 	      <div class="col-md-1">
-	        <img src="musics\cover\UNDIVIDED.jpg" class="img-responsive" style="max-width: 60px">
+	        <img src="musics\cover\NEKKOYA.jpg" class="img-responsive" style="max-width: 60px">
 	      </div>
 	      <div class="col-md-9">
-	        <audio controls style="width: 100%;"><source src="musics/Kangaroo(Prod. ZICO).m4a" type="audio/mpeg"></audio>
+	        <audio controls style="width: 100%;"><source src="musics/PICK ME.mp3" type="audio/mpeg"></audio>
 	      </div>
 	      <div class="col-md-2">
 	      </div>
