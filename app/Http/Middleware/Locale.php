b32e3a736bf59d10298f627551e74635393219b4
Tanakorn Puraram <tanakorn.pur@bru.ac.th>
2018-07-11 15:44:30 +0700

    add musics

---
+++ app/Http/Middleware/Locale.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Locale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(session()->has('locale')){
+          App::setLocale(session()->get('locale'));
+        }else{
+          session('locale', Config::get('app.locale'));
+        }
+        return $next($request);
+    }
+}
